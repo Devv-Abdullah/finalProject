c19cfb9b411dbc0623050dfc11ae9a07c61adfbe
Devv-Abdullah <abdullahalkhatib64@gmail.com>
2024-10-13 19:56:39 +0300

    woowowowowo

--- js/index.js
+++ js/index.js
@@ -56,3 +56,32 @@ RegistrationForm.addEventListener('click', function(event){
     
     localStorage.setItem('userDataStorage', JSON.stringify(allUsersData))
 })
+
+var loginHere = document.getElementById('loginHere');
+loginHere.addEventListener('click', function(){
+    let timerInterval;
+Swal.fire({
+  title: "loading",
+//   html: "I will close in <b></b> milliseconds.",
+  timer: 1000,
+  timerProgressBar: true,
+  didOpen: () => {
+    Swal.showLoading();
+    const timer = Swal.getPopup().querySelector("b");
+    timerInterval = setInterval(() => {
+      timer.textContent = `${Swal.getTimerLeft()}`;
+    }, 100);
+  },
+  willClose: () => {
+    clearInterval(timerInterval);
+  }
+}).then((result) => {
+  /* Read more about handling dismissals below */
+  if (result.dismiss === Swal.DismissReason.timer) {
+    // console.log("I was closed by the timer");
+  }
+});
+      sleep(1000).then(() =>{
+        window.location.href = 'login.html'
+    })
+})
--- js/login.js
+++ js/login.js
@@ -45,3 +45,32 @@ Registrationlogin.addEventListener('submit', function(event){
         }
     }
 })
+
+var register = document.getElementById('register');
+register.addEventListener('click', function(){
+    let timerInterval;
+Swal.fire({
+  title: "loading",
+//   html: "I will close in <b></b> milliseconds.",
+  timer: 1000,
+  timerProgressBar: true,
+  didOpen: () => {
+    Swal.showLoading();
+    const timer = Swal.getPopup().querySelector("b");
+    timerInterval = setInterval(() => {
+      timer.textContent = `${Swal.getTimerLeft()}`;
+    }, 100);
+  },
+  willClose: () => {
+    clearInterval(timerInterval);
+  }
+}).then((result) => {
+  /* Read more about handling dismissals below */
+  if (result.dismiss === Swal.DismissReason.timer) {
+    // console.log("I was closed by the timer");
+  }
+});
+      sleep(1000).then(() =>{
+        window.location.href = 'index.html'
+    })
+})
