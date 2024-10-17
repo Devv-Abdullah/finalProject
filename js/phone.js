0fa04ddbe895ba08a71fdd63bc1257a31661b701
Devv-Abdullah <abdullahalkhatib64@gmail.com>
2024-10-17 19:24:18 +0300

    full project

--- js/phone.js
+++ js/phone.js
@@ -61,3 +61,58 @@ photo6.addEventListener('click', function(event){
         window.location.href = 'photo6.html';
     });
 })
+
+
+var price1 = document.getElementById('price1');
+price1.addEventListener('click', function(event){
+    event.preventDefault();
+
+    sleep(1000).then(() => {
+        window.location.href = 'credit1.html';
+    });
+})
+
+var price2 = document.getElementById('price2');
+price2.addEventListener('click', function(event){
+    event.preventDefault();
+
+    sleep(1000).then(() => {
+        window.location.href = 'credit2.html';
+    });
+})
+
+var price3 = document.getElementById('price3');
+price3.addEventListener('click', function(event){
+    event.preventDefault();
+
+    sleep(1000).then(() => {
+        window.location.href = 'credit3.html';
+    });
+})
+
+var price4 = document.getElementById('price4');
+price4.addEventListener('click', function(event){
+    event.preventDefault();
+
+    sleep(1000).then(() => {
+        window.location.href = 'credit4.html';
+    });
+})
+
+var price5 = document.getElementById('price5');
+price5.addEventListener('click', function(event){
+    event.preventDefault();
+
+    sleep(1000).then(() => {
+        window.location.href = 'credit5.html';
+    });
+})
+
+var price6 = document.getElementById('price6');
+price6.addEventListener('click', function(event){
+    event.preventDefault();
+
+    sleep(1000).then(() => {
+        window.location.href = 'credit6.html';
+    });
+})
