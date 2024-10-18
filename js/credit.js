b87579e1aa070bba686671348ceef25991d8774d
Devv-Abdullah <abdullahalkhatib64@gmail.com>
2024-10-18 11:15:09 +0300

    full project

---
+++ js/credit.js
@@ -0,0 +1,26 @@
+// var submit = document.getElementById('submit');
+// var card = document.getElementById('card');
+// var years = document.getElementById('years');
+// var cvv = document.getElementById('cvv');
+// var nameOnTheCard = document.getElementById('nameOnTheCard');
+
+// submit.addEventListener('click', function(event){
+//     event.preventDefault();
+
+//     if(card.value == [] || years.value == [] || cvv.value == [] || nameOnTheCard.value == []){
+//         Swal.fire({
+//             icon: "error",
+//             title: "Enter  fields are required",
+//             text: "Please fill in all the fields!",
+//           });
+//     }
+//     else if(password.value == repeatPassword.value){
+//         Swal.fire({
+//             position: "top-center",
+//             icon: "success",
+//             title: "The operation was Successful",   
+//             showConfirmButton: false,
+//             timer: 2500
+//           });
+//     }
+// })
